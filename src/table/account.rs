d6c9844b4137bec888e2c1d22be46b5bceb7525e
Daniel Melody <dmwalter12@gmail.com>
2020-05-15 15:53:49 -0400

    adding edit & delete to account

--- src/table/account.rs
+++ src/table/account.rs
@@ -35,13 +35,46 @@ impl Account {
             value: 0.0,
         }
     }
+
+    pub fn find(accounts: &Vec<Account>) -> i32 {
+        let arg = cli::get_input("ID");
+        let mut index = 0;
+        for acc in accounts {
+            if acc.simplify_id().contains(&arg) {
+                return index;
+            }
+            index += 1;
+        }
+        -1
+    }
+
+    pub fn edit(&mut self) {
+        let field = cli::get_input("Field to edit");
+        let name_field = String::from("name");
+        let value_field = String::from("value");
+        match field {
+            name_field => self.name = cli::get_input("Name"),
+            value_field => self.value = cli::try_into_money(&mut cli::get_input("Value")),
+            _ => (),
+        }
+    }
+
+    fn simplify_id(&self) -> String {
+        let id = Simple::from_uuid(self.id);
+        let mut id = id.to_string();
+        let (id_string, _extra) = id.split_at_mut(6);
+        id_string.to_string()
+    }
 }
 
 impl fmt::Display for Account {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let id = Simple::from_uuid(self.id);
-        let mut id = id.to_string();
-        let (id_string, _extra) = id.split_at_mut(6);
-        write!(f, "{},\t{}\t\t{}", id_string, self.name, self.value)
+        write!(
+            f,
+            "{},\t{}\t\t{}",
+            self.simplify_id(),
+            self.name,
+            self.value
+        )
     }
 }
